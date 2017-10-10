f378d40efc80bea77265c8f203440afb77893110
Craig Ramey <craigramey@AxisAdmins-iMac.local>
2017-10-10 16:31:33 -0400

    deleteTodo implemented

--- tasks.go
+++ tasks.go
@@ -27,6 +27,34 @@ func (t Todo) toString() string {
 	return string(bytes)
 }
 
+//TODO: updateTodo function
+//TODO: deleteTodo function
+func deleteTodo(n int) error {
+	todos := getTodos()
+	var updatedTodos []Todo
+	for i, todo := range todos {
+		num := i + 1
+		if num != n {
+			updatedTodos = append(updatedTodos, todo)
+		}
+	}
+
+	bytes, err := json.Marshal(updatedTodos)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	path, err := getPath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file := filepath.Join(path, ".todos.json")
+	ioutil.WriteFile(file, bytes, 0755)
+	return nil
+}
+
 func addTodo(t string) error {
 	todos := getTodos()
 	todo := Todo{Task: t, IsComplete: false}
@@ -89,11 +117,12 @@ func main() {
 	red := color.New(color.FgRed).SprintFunc()
 	todos := getTodos()
 
-	for _, todo := range todos {
+	for i, todo := range todos {
+		num := i + 1
 		if todo.IsComplete == true {
-			fmt.Printf("%s %s\n", blue("[X]"), red(todo.Task))
+			fmt.Printf("%d. %s %s\n", num, blue("[X]"), red(todo.Task))
 		} else {
-			fmt.Printf("%s %s\n", blue("[ ]"), red(todo.Task))
+			fmt.Printf("%d. %s %s\n", num, blue("[ ]"), red(todo.Task))
 		}
 	}
 }
