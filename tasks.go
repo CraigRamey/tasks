a7b614c88ceb53c8b3aabceec8045cce37f769fe
Craig Ramey <craigramey@AxisAdmins-iMac.local>
2017-10-10 14:50:58 -0400

    addTodo function working properly

--- tasks.go
+++ tasks.go
@@ -10,7 +10,7 @@ import (
 	"os/user"
 	"path/filepath"
 
-	"github.com/beego/bee/logger/colors"
+	"github.com/fatih/color"
 )
 
 type Todo struct {
@@ -25,6 +25,26 @@ func (t Todo) toString() string {
 		os.Exit(1)
 	}
 	return string(bytes)
+}
+
+func addTodo(t string) error {
+	todos := getTodos()
+	todo := Todo{Task: t, IsComplete: false}
+	todos = append(todos, todo)
+	bytes, err := json.Marshal(todos)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	path, err := getPath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file := filepath.Join(path, ".todos.json")
+	ioutil.WriteFile(file, bytes, 0755)
+	return nil
 }
 
 func getTodos() []Todo {
@@ -55,13 +75,25 @@ func getPath() (string, error) {
 }
 
 func main() {
+	path, err := getPath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file := filepath.Join(path, ".todos.json")
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		ioutil.WriteFile(file, nil, 0755)
+	}
+
+	blue := color.New(color.FgBlue).SprintFunc()
+	red := color.New(color.FgRed).SprintFunc()
 	todos := getTodos()
+
 	for _, todo := range todos {
 		if todo.IsComplete == true {
-			fmt.Printf("%s is complete", colors.Blue(todo.Task))
+			fmt.Printf("%s %s\n", blue("[X]"), red(todo.Task))
 		} else {
-			fmt.Printf("%s is not yet complete", colors.Red(todo.Task))
+			fmt.Printf("%s %s\n", blue("[ ]"), red(todo.Task))
 		}
-		fmt.Println(todo.toString())
 	}
 }
