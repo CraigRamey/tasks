24e4b1a4f0dfe2cb07feec70d02fdcb41f7c6694
Craig Ramey <craigramey@AxisAdmins-iMac.local>
2017-10-11 10:55:58 -0400

    have update working

--- tasks.go
+++ tasks.go
@@ -27,8 +27,39 @@ func (t Todo) toString() string {
 	return string(bytes)
 }
 
-//TODO: updateTodo function
-//TODO: deleteTodo function
+func updateTodo(n int, s string, b bool) error {
+	todos := getTodos()
+	var updatedTodos []Todo
+	if n+1 > len(todos) {
+		return errors.New("Todo not found, nothing updated")
+	}
+	for i, todo := range todos {
+		num := i + 1
+		if num == n {
+			single := &todo
+			single.Task = s
+			single.IsComplete = b
+		}
+		updatedTodos = append(updatedTodos, todo)
+	}
+
+	bytes, err := json.Marshal(updatedTodos)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	path, err := getPath()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	file := filepath.Join(path, ".todos.json")
+	ioutil.WriteFile(file, bytes, 0755)
+	return nil
+}
+
 func deleteTodo(n int) error {
 	todos := getTodos()
 	var updatedTodos []Todo
@@ -37,6 +68,9 @@ func deleteTodo(n int) error {
 		if num != n {
 			updatedTodos = append(updatedTodos, todo)
 		}
+	}
+	if len(todos) == len(updatedTodos) {
+		return errors.New("Todo not found, nothing deleted")
 	}
 
 	bytes, err := json.Marshal(updatedTodos)
@@ -115,6 +149,7 @@ func main() {
 
 	blue := color.New(color.FgBlue).SprintFunc()
 	red := color.New(color.FgRed).SprintFunc()
+	updateTodo(1, "Update Todos", false)
 	todos := getTodos()
 
 	for i, todo := range todos {
@@ -125,4 +160,5 @@ func main() {
 			fmt.Printf("%d. %s %s\n", num, blue("[ ]"), red(todo.Task))
 		}
 	}
+
 }
